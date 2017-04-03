5fcf15118715f8c6e0303ad5bd8765358d4927aa
Dawid Wełna <jan125djw@gmail.com>
2017-04-03 15:46:57 +0200

    ko

--- kol2.py
+++ kol2.py
@@ -14,3 +14,63 @@
 # If you have enough courage and time, try storing (reading/writing) 
 # data in text files (YAML, JSON).
 # If you have even more courage, try implementing user interface.
+
+
+import math
+
+
+class Student:
+	
+	def __init__(self, name, surname, id):
+		self.name = name
+		self.surname = surname
+		self.id = id
+	
+	def create_student(self, name, surname, id):
+		self.name = name
+		self.surname = surname
+		self.id = id
+
+	def speak(self):
+		print "Hello! I am {} {}, my id number is {}".format(self.name, self.surname, self.id)
+		
+		
+
+class Subject(Student):
+	
+	def __init__(self, subject_name, teacher):
+		self.subject_name = subject_name
+		self.teacher = teacher
+		
+	def speak(self):
+		print "This subject is called {}".format(self.subject_name)
+
+		
+class Grades(Subject):
+	
+	attendance = 0
+	total_attendance = 14
+	
+	def __init__(self, grade, lesson_number):
+		self.lesson_number = lesson_number
+		self.grade = grade
+	
+	def addGrade(self, grade, lesson_number):
+		self.lesson_number = lesson_number
+		self.grade = grade
+		
+	def count_attendance(self):
+		self.attendance = self.attendance + 1
+		
+		
+		
+		
+def main():
+	s1 = Student('Andrzej', 'Rdad', 1)
+	s1.speak()
+
+	sub1 = Subject('Literature', 'Arthur C. Doyle')
+	sub1.speak()
+	
+	
+main()
